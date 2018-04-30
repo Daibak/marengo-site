096d37e41842f2b6df5cd8d3e3c7c4f088389b21
Daibak <gdballa@gmail.com>
2018-04-30 19:17:32 +0200

    small adaptation to project

--- templates/pre-push.py
+++ templates/pre-push.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python
 
-import datetime
-
 import pysftp
 
 # FTP server connection strings
@@ -10,15 +8,10 @@ sftp_username = "me_avvocatodoraballabio"
 sftp_key = "~/.ssh/nfs_melissa_rsa"
 sftp_key_pw = ''
 
-# timestamp for backup
-timestamp = '{:%Y-%m-%d %H:%M:%S}'.format(datetime.datetime.now())
-
 # Local paths
 local_dir = "www"
 
 # remote server paths
-dest_dir = "/home/tmp/.deploy-" + timestamp
-backup_dir = "/private/public-" + timestamp
 production_dir = "/home/public"
 
 if __name__ == '__main__':
@@ -29,21 +22,5 @@ if __name__ == '__main__':
                            private_key=sftp_key, private_key_pass=sftp_key_pw) as sftp_conn:
         print("Starting directory transfer of " + local_dir)
         sftp_conn.put_r(local_dir, production_dir)
-        # print("Starting directory transfer of " + local_dir)
-        # sftp_conn.put_r(local_dir, dest_dir)
-
-        # print("Renaming " + production_dir + " to " + backup_dir)
-        # sftp_conn.rename(production_dir, backup_dir)
-
-        # all_backups = sftp_conn.listdir(backup_dir)
-
-        # if all_backups is not None:
-        #    print("Removing oldest backup")
-        #    oldest_subdir = min(
-        #        all_backups, key=lambda d: sftp_conn.stat(d).st_mtime)
-        #    sftp_conn.remove(oldest_subdir)
-
-        # print("Renaming " + dest_dir + " to " + production_dir)
-        # sftp_conn.rename(dest_dir, production_dir)
 
     print("Transfer complete")
